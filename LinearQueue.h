32a6797237d6d54978d773de59f784b23b07bf03
Rajuarshad <154614126+iqraarshad199@users.noreply.github.com>
2024-11-22 20:10:46 +0500

    Add files via upload
    Exercise 1

---
+++ LinearQueue.h
@@ -0,0 +1,52 @@
+#pragma once
+
+//class LinearQueue
+//{
+//private:
+//	int* arr;
+//	int front;
+//	int rear;
+//	int size;
+//public:
+//
+//	LinearQueue(int s);
+//
+//	/*
+//	@Check if the queue is empty.
+//	*/
+//	bool isEmpty();
+//
+//	/*
+//	@Check if the queue is full.
+//	*/
+//	bool isFull();
+//
+//	/*
+//	** Enqueue operation to add an element to the rear of the queue.
+//	** If the queue is empty both pointers are at their initial state, it sets front to 0.
+//	@val will store the value at the end of the queue.
+//	*/
+//	void Enqueue(int value);
+//
+//	/*
+//	** Dequeue operation to remove an element from the front of the queue.
+//	@Return the value after removing from the front.
+//	*/
+//	int Dequeue();
+//
+//	/*
+//	@Returns the front element of the queue without removing it.
+//	*/
+//	int getFront();
+//
+//	/*
+//	@Prints all elements in the queue from front to rear.
+//	*/
+//	void display();
+//
+//	/*
+//	@Destructor to free allocated memory.
+//	*/
+//	~LinearQueue();
+//};
+
