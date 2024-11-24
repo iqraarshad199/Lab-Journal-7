8ca0feaf43e297f113838d31e5f470397dd7b7f3
Rajuarshad <154614126+iqraarshad199@users.noreply.github.com>
2024-11-25 01:49:14 +0500

    Add files via upload

---
+++ Exercise 3/LinearQueue.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "Node.h"
+class LinearQueue
+{
+private:
+	Node* front;
+	Node* rear;
+public:
+    /*
+    @Constructor
+    */
+	LinearQueue();
+
+    /*
+    @Destructor
+    */
+    ~LinearQueue();
+
+    /*
+    @Check if the queue is empty.
+    */
+    bool isEmpty();
+   /*
+    @value: The integer value to be added to the queue.
+   */
+    void Enqueue(int value); 
+
+    /*
+    **Removes and returns the front element from the queue.
+    * Moves the front pointer to the next node in the list.
+    */
+    int Dequeue();     
+
+    /*
+    **Returns the front element of the queue without removing it.
+    */
+    int getFront();       
+
+    /*
+    @Prints all elements currently in the queue.
+    @If the queue is empty, it prints Queue is empty.
+    */
+    void display();
+};
+
